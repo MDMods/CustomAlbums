8cb519140b442abc34d0cd2755470cfbcb460202
Super Balls <superballs15@outlook.com>
2023-11-04 15:09:40 -0700

    Added LongChartPatch and WebApiPatch

---
+++ Patches/LongChartPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Il2CppDYUnityLib;
+
+namespace CustomAlbums.Patches
+{
+    /// <summary>
+    /// Changes the maximum length of a chart from 4 minutes to about 357913 minutes.
+    /// </summary>
+    [HarmonyPatch(typeof(FixUpdateTimer), "Run")]
+    internal class LongChartPatch
+    {
+        private static void Prefix(FixUpdateTimer __instance)
+        {
+            if (__instance.totalTick >= 24000 && __instance.totalTick < int.MaxValue)
+            {
+                __instance.totalTick = int.MaxValue;
+            }
+        }
+    }
+}
---
+++ Patches/WebApiPatch.cs
@@ -0,0 +1,42 @@
+﻿using CustomAlbums.Managers;
+using CustomAlbums.Utilities;
+using HarmonyLib;
+using Il2CppAccount;
+using Il2CppAssets.Scripts.Database;
+
+namespace CustomAlbums.Patches
+{
+    internal class WebApiPatch
+    {
+        /// <summary>
+        /// Patches the SendToUrl method to not attempt to send stats or high scores of custom charts to the official server.
+        /// </summary>
+        [HarmonyPatch(typeof(GameAccountSystem), nameof(GameAccountSystem.SendToUrl))]
+        internal class SendToUrlPatch
+        {
+            private static readonly Logger Logger = new("WebApiPatch");
+            private static bool Prefix(string url, string method, Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> datas)
+            {
+
+                switch (url)
+                {
+                    case "statistics/pc-play-statistics-feedback":
+                        if (datas["music_uid"].ToString().StartsWith($"{AlbumManager.Uid}"))
+                        {
+                            Logger.Msg("[SendToUrlPatch] Blocked play feedback upload:" + datas["music_uid"].ToString());
+                            return false;
+                        }
+                        break;
+                    case "musedash/v2/pcleaderboard/high-score":
+                        if (GlobalDataBase.dbBattleStage.musicUid.StartsWith($"{AlbumManager.Uid}"))
+                        {
+                            Logger.Msg("[SendToUrlPatch] Blocked high score upload:" + GlobalDataBase.dbBattleStage.musicUid);
+                            return false;
+                        }
+                        break;
+                }
+                return true;
+            }
+        }
+    }
+}
