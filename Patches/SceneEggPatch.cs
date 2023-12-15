9f8ef3f7bd754dcd713154fa15093ecf5ba81de7
Super Balls <superballs15@outlook.com>
2023-12-14 16:45:10 -0800

    Fixed hierarchical issue with SceneEggs

--- Patches/SceneEggPatch.cs
+++ Patches/SceneEggPatch.cs
@@ -28,12 +28,20 @@ namespace CustomAlbums.Patches
             private static readonly Logger Logger = new(nameof(ControllerPatch));
             private static void Prefix(Il2CppSystem.Collections.Generic.List<int> sceneEggIdsBuffer)
             {
+                // If the chart is not custom then leave
                 var uid = DataHelper.selectedMusicUid;
-                if (!uid.StartsWith("999-")) return;
-
+                if (!uid.StartsWith($"{AlbumManager.UID}-")) return;
+                
+                // If the album doesn't exist (?) or if there are no SceneEggs then leave
                 var album = AlbumManager.GetByUid(uid);
                 if (album is null || album.Info.SceneEgg == SceneEggs.None) return;
+                
+                // Adds the scene egg to the buffer
                 sceneEggIdsBuffer.Add((int)album.Info.SceneEgg);
+
+                // Removes all scene eggs from the buffer that are not the one that was added
+                // This prevents a hierarchical issue where character choice would be used over chart choice
+                sceneEggIdsBuffer.RemoveAll((Il2CppSystem.Predicate<int>)(eggId => eggId != (int)album.Info.SceneEgg));
             }
         }
     }
