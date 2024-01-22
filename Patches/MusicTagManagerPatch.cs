4c51c77358633678a5cb867327e4474f37313599
Super Balls <superballs15@outlook.com>
2024-01-21 17:40:44 -0800

    Added performance increase on loading tag menu (thanks TGM!)

---
+++ Patches/MusicTagManagerPatch.cs
@@ -0,0 +1,25 @@
+﻿using Il2Cpp;
+using Il2CppAssets.Scripts.Database;
+using HarmonyLib;
+using Il2CppAssets.Scripts.PeroTools.Commons;
+using Il2CppAssets.Scripts.PeroTools.Managers;
+
+namespace CustomAlbums.Patches
+{
+    internal class MusicTagManagerPatch
+    {
+        /// <summary>
+        ///     Makes the game think (correctly) that there are not 1000 albums upon loading the tag menu.
+        ///     Increases performance substantially upon loading the tag menu.
+        /// </summary>
+        [HarmonyPatch(typeof(MusicTagManager), nameof(MusicTagManager.InitDatas))]
+        internal static class Fix1000AlbumsPatch
+        {
+            private static void Postfix()
+            {
+                var configObject = Singleton<ConfigManager>.instance.GetConfigObject<DBConfigAlbums>();
+                configObject.m_MaxAlbumUid = configObject.count - 3;
+            }
+        }
+    }
+}
