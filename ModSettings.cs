6e9cb729621f4580f8bfe93b4a44f18aa248c223
Super Balls <superballs15@outlook.com>
2024-02-27 20:39:02 -0800

    CAM-50 Disabled vanilla UnityEngine logging by default as it is mostly useless and can take up a lot of space

--- ModSettings.cs
+++ ModSettings.cs
@@ -6,8 +6,10 @@ namespace CustomAlbums
     {
         private static MelonPreferences_Entry<bool> _verboseLogging;
         private static MelonPreferences_Entry<bool> _savingEnabled;
+        private static MelonPreferences_Entry<bool> _enableLogWriteToFile;
         public static bool VerboseLogging => _verboseLogging.Value;
         public static bool SavingEnabled => _savingEnabled.Value;
+        public static bool LoggingToFileEnabled => _enableLogWriteToFile.Value;
 
 
         internal static void Register()
@@ -16,6 +18,7 @@ namespace CustomAlbums
 
             _verboseLogging = category.CreateEntry("VerboseLogging", false, "Verbose Logging");
             _savingEnabled = category.CreateEntry("SavingEnabled", true, "Enable Saving");
+            _enableLogWriteToFile = category.CreateEntry("LogWriteToFile", false, "Enable Log Write to File");
         }
     }
 }
