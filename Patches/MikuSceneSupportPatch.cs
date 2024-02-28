a7aaf9e8ce3b61da893c31b2883be06a294f4e4b
Super Balls <superballs15@outlook.com>
2024-02-28 13:26:00 -0800

    CAM-52 Fixes issue in vanilla game where scene switching to the Miku scene doesn't correctly change the boss or background

---
+++ Patches/MikuSceneSupportPatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomAlbums.Utilities;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppGameLogic;
+
+namespace CustomAlbums.Patches
+{
+    internal class MikuSceneSupportPatch
+    {
+
+        /// <summary>
+        ///     Fixes the purple background issue when using the BMS ID "1X" to switch to the Miku scene.
+        /// </summary>
+        [HarmonyPatch(typeof(GameMusicScene), nameof(GameMusicScene.SceneFestival))]
+        internal class MikuFixPatch
+        {
+            private static void Finalizer(string sceneFestivalName, ref string __result)
+            {
+                
+                if (sceneFestivalName.AsSpan(6).Length <= 2 ||
+                    !int.TryParse(sceneFestivalName.AsSpan(6).ToString(), out var sceneIndex)) return;
+                
+                // For some reason, scene change 1X sets scene to scene_010, which is invalid
+                // Just trim off "scene_" and parse the numbers after the underscore to int again to get 10 instead of 010
+                __result = $"scene_{sceneIndex}";
+            }
+        }
+    }
+}
