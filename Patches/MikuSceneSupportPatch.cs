5fae51edd8f6a6bcfbedd5a2b8104448401e9f31
Super Balls <superballs15@outlook.com>
2024-02-28 13:33:12 -0800

    Removed useless imports

--- Patches/MikuSceneSupportPatch.cs
+++ Patches/MikuSceneSupportPatch.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CustomAlbums.Utilities;
-using HarmonyLib;
-using Il2Cpp;
+﻿using HarmonyLib;
 using Il2CppGameLogic;
 
 namespace CustomAlbums.Patches
