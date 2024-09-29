97304b15e2400fc3d92aa4eb23beca9c61a75624
AshtonMemer <53633401+AshtonMemer@users.noreply.github.com>
2024-09-29 04:57:32 -0500

    Bump to v4.1.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.2.0")]
-[assembly: AssemblyFileVersion("4.1.2.0")]
-[assembly: MelonInfo(typeof(CustomAlbums.Main), "CustomAlbums", "4.1.2", "Team Baller")]
+[assembly: AssemblyVersion("4.1.3.0")]
+[assembly: AssemblyFileVersion("4.1.3.0")]
+[assembly: MelonInfo(typeof(CustomAlbums.Main), "CustomAlbums", "4.1.3", "Team Baller")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 0, 255, 150)]
