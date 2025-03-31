cbe7a254510a49e380021b0823313c89f5255628
ALLMarvelous <53633401+ALLMarvelous@users.noreply.github.com>
2025-03-31 17:35:21 -0500

    Make Melon info more accessible

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,23 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using MelonLoader;
+using Main = CustomAlbums.Main;
 
-[assembly: AssemblyTitle("CustomAlbums")]
+[assembly: AssemblyTitle(Main.MelonName)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomAlbums")]
+[assembly: AssemblyProduct(Main.MelonName)]
 [assembly: AssemblyCopyright("Copyright © Muse Dash Modding Community 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8ea4daf8-4ffd-465f-9b07-ac6925c6453f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.4.0")]
-[assembly: AssemblyFileVersion("4.1.4.0")]
-[assembly: MelonInfo(typeof(CustomAlbums.Main), "CustomAlbums", "4.1.4", "Two Fellas")]
+[assembly: AssemblyVersion($"{Main.MelonVersion}.0")]
+[assembly: AssemblyFileVersion($"{Main.MelonVersion}.0")]
+[assembly: MelonInfo(typeof(Main), Main.MelonName, Main.MelonVersion, Main.MelonAuthor)]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 0, 255, 150)]
