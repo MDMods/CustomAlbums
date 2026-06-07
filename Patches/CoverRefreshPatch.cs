6b04daeed3527ec6855b8dd672c15b9a1da1bfb8
ALLMarvelous <53633401+ALLMarvelous@users.noreply.github.com>
2026-06-07 01:17:48 -0500

    Help cover arts refresh during hot reload

---
+++ Patches/CoverRefreshPatch.cs
@@ -0,0 +1,42 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using Il2Cpp;
+
+namespace CustomAlbums.Patches
+{
+    internal static class CoverRefreshPatch
+    {
+        private static readonly Dictionary<MusicStageCell, string> ActiveCells = new();
+
+        [HarmonyPatch(typeof(MusicStageCell), nameof(MusicStageCell.SetCoverLogic))]
+        internal static class SetCoverLogicPatch
+        {
+            private static void Postfix(MusicStageCell __instance, MusicStageCell.MusicStageCellInfo cellInfo)
+            {
+                ActiveCells[__instance] = cellInfo.musicUid;
+            }
+        }
+
+        [HarmonyPatch(typeof(MusicStageCell), nameof(MusicStageCell.OnDestroy))]
+        internal static class OnDestroyPatch
+        {
+            private static void Postfix(MusicStageCell __instance)
+            {
+                ActiveCells.Remove(__instance);
+            }
+        }
+
+        internal static void Refresh(string musicUid)
+        {
+            // Iterate over a copy to avoid enumeration modification exceptions if a cell destroys itself
+            var cells = new Dictionary<MusicStageCell, string>(ActiveCells);
+            foreach (var kvp in cells)
+            {
+                if (kvp.Value == musicUid && kvp.Key != null && kvp.Key.gameObject.activeInHierarchy)
+                {
+                    kvp.Key.ForceRefreshData();
+                }
+            }
+        }
+    }
+}
